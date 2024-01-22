0b17efb0651121fdd91e7f0a0a3a7b1f1914e24d
Igor Astashov <astashovivl@gmail.com>
2024-01-22 09:35:03 +0700

    1-ю страницу -'Исследование'

--- app.py
+++ app.py
@@ -1,102 +1,131 @@
 import pandas as pd
 import streamlit as st
 from PIL import Image
+import altair as alt
 from model import open_data, preprocess_data, split_data, load_model_and_predict
 
 
-def process_main_page():
-    show_main_page()
-    process_side_bar_inputs()
+image = Image.open('data/image.png')
+
+st.set_page_config(
+    layout="wide",
+    initial_sidebar_state="auto",
+    page_title="Promo Bank",
+    page_icon=image
+)
+
+image = Image.open('data/image.png')
+
+st.title(
+    """
+    Предсказание отклика клиента на промо банка
+    Определяем, кто из клиентов откликнется на предложение банка, а кто – нет.
+    """
+)
+
+st.image(image)
+
+"""
+Таблица 1: Исследование
+"""
 
 
-def show_main_page():
-    image = Image.open('data/image.png')
 
-    st.set_page_config(
-        layout="wide",
-        initial_sidebar_state="auto",
-        page_title="Promo Bank",
-        page_icon=image,
+def bar_chart(source: pd.DataFrame,
+              feature: str,
+              color: str,
+              bin: alt.Bin = None,
+              x_title: str = None,
+              y_title: str = None) -> None:
+    """
+    Function draws and displays bar chart.
 
-    )
+    :param source: data to display
+    :param feature: feature to visualize
+    :param color: bars color
+    :param bin: binarize data params
+    :param x_title: x label title to display
+    :param y_title: y label title to display
+    """
+    chart = alt.Chart(source).transform_joinaggregate(
+        total='sum(count)',
+    ).transform_calculate(
+        percent="datum.count / datum.total"
+    ).mark_bar(color=color).encode(
+        alt.X(feature, bin=bin, axis=alt.Axis(labelAngle=0, title=x_title)),
+        alt.Y('sum(percent):Q', axis=alt.Axis(format='.0%', title=y_title))
+    ).properties(height=250)
 
-    st.write(
-        """
-        # Предсказание отклика клиента на промо банка
-        Определяем, кто из клиентов откликнется на предложение банка, а кто – нет.
-        """
-    )
+    st.altair_chart(chart, use_container_width=True)
 
-    st.image(image)
+def plot_age(df):
+    st.subheader('Возраст')
+    source = df.AGE.value_counts().reset_index()
+    bar_chart(source, 'AGE:Q', color='#83c9ff', bin=alt.Bin(maxbins=10))
 
+def plot_postal_adress(df):
+    st.subheader('Почтовый адрес')
+    source = df.POSTAL_ADDRESS_PROVINCE.value_counts().reset_index()
+    bar_chart(source, 'POSTAL_ADDRESS_PROVINCE:N', color='#83c9ff', y_title='percent')
 
-def write_user_data(df):
-    st.write("## Ваши данные")
-    st.write(df)
-
-
-def write_prediction(prediction, prediction_probas):
-    st.write("## Предсказание")
-    st.write(prediction)
-
-    st.write("## Вероятность предсказания")
-    st.write(prediction_probas)
-
-
-def process_side_bar_inputs():
-    st.sidebar.header('Заданные пользователем параметры')
-    user_input_df = sidebar_input_features()
-
-    train_df = open_data()
-    train_X_df, _ = split_data(train_df)
-    full_X_df = pd.concat((user_input_df, train_X_df), axis=0)
-    preprocessed_X_df = preprocess_data(full_X_df, test=False)
-
-    user_X_df = preprocessed_X_df[:1]
-    write_user_data(user_X_df)
-
-    prediction, prediction_probas = load_model_and_predict(user_X_df)
-    write_prediction(prediction, prediction_probas)
+def plot_education(df):
+    st.subheader('Образование')
+    source = df.EDUCATION.value_counts().reset_index()
+    bar_chart(source, 'EDUCATION:N', color='#83c9ff', y_title='percent')
 
 
-def sidebar_input_features():
-    age = st.sidebar.slider("Возраст", min_value=1, max_value=80, value=30,
+
+
+
+
+"""
+Таблица 2: Предсказание
+"""
+
+def input_features():
+    age = st.slider("Возраст", min_value=1, max_value=80, value=30,
                             step=1)
-    postal_address = st.sidebar.selectbox("Почтовый адрес", (
-       'Оренбургская область', 'Кабардино-Балкария', 'Иркутская область',
-       'Ростовская область', 'Белгородская область',
-       'Вологодская область', 'Волгоградская область',
-       'Ярославская область', 'Краснодарский край', 'Карелия',
-       'Хабаровский край', 'Калужская область', 'Еврейская АО',
-       'Красноярский край', 'Свердловская область', 'Камчатская область',
-       'Алтайский край', 'Липецкая область', 'Адыгея',
-       'Челябинская область', 'Мурманская область', 'Амурская область',
-       'Ленинградская область', 'Тверская область', 'Тульская область',
-       'Воронежская область', 'Кемеровская область', 'Башкирия',
-       'Пермская область', 'Омская область', 'Саратовская область',
-       'Мордовская республика', 'Новосибирская область',
-       'Санкт-Петербург', 'Магаданская область', 'Ханты-Мансийский АО',
-       'Псковская область', 'Самарская область', 'Орловская область',
-       'Ивановская область', 'Читинская область', 'Татарстан', 'Якутия',
-       'Кировская область', 'Астраханская область', 'Коми',
-       'Приморский край', 'Чувашия', 'Ставропольский край',
-       'Костромская область', 'Курская область', 'Смоленская область',
-       'Тюменская область', 'Хакасия', 'Брянская область',
-       'Архангельская область', 'Московская область',
-       'Пензенская область', 'Тамбовская область', 'Курганская область',
-       'Владимирская область', 'Ульяновская область', 'Бурятия',
-       'Томская область', 'Нижегородская область',
-       'Калининградская область', 'Удмуртия', 'Рязанская область',
-       'Карачаево-Черкесия', 'Сахалинская область', 'Горный Алтай',
-       'Москва', 'Марийская республика', 'Новгородская область',
-       'Ямало-Ненецкий АО', 'Агинский Бурятский АО',
-       'Усть-Ордынский Бурятский АО', 'Эвенкийский АО', 'Северная Осетия',
-       'Калмыкия'))
-    work_time = st.sidebar.number_input("Время работы на текущем месте (в месяцах)", 12)
-    education = st.sidebar.selectbox("Образование", (
+
+    postal_address = st.selectbox("Почтовый адрес", (
+        'Оренбургская область', 'Кабардино-Балкария', 'Иркутская область',
+        'Ростовская область', 'Белгородская область',
+        'Вологодская область', 'Волгоградская область',
+        'Ярославская область', 'Краснодарский край', 'Карелия',
+        'Хабаровский край', 'Калужская область', 'Еврейская АО',
+        'Красноярский край', 'Свердловская область', 'Камчатская область',
+        'Алтайский край', 'Липецкая область', 'Адыгея',
+        'Челябинская область', 'Мурманская область', 'Амурская область',
+        'Ленинградская область', 'Тверская область', 'Тульская область',
+        'Воронежская область', 'Кемеровская область', 'Башкирия',
+        'Пермская область', 'Омская область', 'Саратовская область',
+        'Мордовская республика', 'Новосибирская область',
+        'Санкт-Петербург', 'Магаданская область', 'Ханты-Мансийский АО',
+        'Псковская область', 'Самарская область', 'Орловская область',
+        'Ивановская область', 'Читинская область', 'Татарстан', 'Якутия',
+        'Кировская область', 'Астраханская область', 'Коми',
+        'Приморский край', 'Чувашия', 'Ставропольский край',
+        'Костромская область', 'Курская область', 'Смоленская область',
+        'Тюменская область', 'Хакасия', 'Брянская область',
+        'Архангельская область', 'Московская область',
+        'Пензенская область', 'Тамбовская область', 'Курганская область',
+        'Владимирская область', 'Ульяновская область', 'Бурятия',
+        'Томская область', 'Нижегородская область',
+        'Калининградская область', 'Удмуртия', 'Рязанская область',
+        'Карачаево-Черкесия', 'Сахалинская область', 'Горный Алтай',
+        'Москва', 'Марийская республика', 'Новгородская область',
+        'Ямало-Ненецкий АО', 'Агинский Бурятский АО',
+        'Усть-Ордынский Бурятский АО', 'Эвенкийский АО', 'Северная Осетия',
+        'Калмыкия'))
+
+    education = st.selectbox("Образование", (
         'Среднее специальное', 'Среднее', 'Неполное среднее', 'Высшее',
         'Неоконченное высшее', 'Два и более высших образования', 'Ученая степень'))
-    gen_industry = st.sidebar.selectbox("Класс (если вы пенсионер то: 'not defined')", (
+
+    child_total = st.slider(
+        "Количество детей",
+        min_value=0, max_value=10, value=0, step=1)
+
+    gen_industry = st.selectbox("Сфера деятельности (если клиент пенсионер то: 'not defined')", (
         'Торговля', 'Информационные технологии', 'Образование',
         'Государственная служба', 'Другие сферы', 'Сельское хозяйство',
         'Здравоохранение', 'Металлургия/Промышленность/Машиностроение',
@@ -110,17 +139,19 @@ def sidebar_input_features():
         'Юридические услуги/нотариальные услуги', 'Маркетинг',
         'Подбор персонала', 'Информационные услуги', 'Недвижимость',
         'Управляющая компания', 'Логистика', 'not defined'))
-    child_total = st.sidebar.slider(
-        "Количество детей",
-        min_value=0, max_value=10, value=0, step=1)
-    personal_income = st.sidebar.number_input("Персональный доход", 40000)
-    closed_fl = st.sidebar.selectbox("Текущий статус кредита", ("Закрыт", "Не закрыт"))
+
+    work_time = st.number_input("Время работы на текущем месте (в месяцах)", 12)
+
+# st.divider()
+
+    personal_income = st.number_input("Персональный доход", 40000)
+
+    closed_fl = st.selectbox("Текущий статус кредита", ("Закрыт", "Не закрыт"))
 
     translatetion = {
         "Закрыт": 1,
         "Не закрыт": 0
     }
-
     data = {
         "AGE": age,
         "POSTAL_ADDRESS_PROVINCE": postal_address,
@@ -137,5 +168,59 @@ def sidebar_input_features():
     return df
 
 
+def write_prediction(prediction, prediction_probas):
+    st.write("## Предсказание")
+    st.write(prediction)
+    st.write("## Вероятность предсказания")
+    st.write(prediction_probas)
+
+def compute_tab1():
+    st.write("""
+            ### Введите данные клиента:
+            """
+             )
+
+
+def write_user_data(df):
+    st.write("## Введенные данные")
+    st.write(df)
+
+def write_prediction(prediction, prediction_probas):
+    st.write("## Предсказание")
+    st.write(prediction)
+
+    st.write("## Вероятность предсказания")
+    st.write(prediction_probas)
+
+def process_side_bar_inputs():
+    train_df = open_data()
+    train_X_df, _ = split_data(train_df)
+    full_X_df = pd.concat((user_input_df, train_X_df), axis=0)
+    preprocessed_X_df = preprocess_data(full_X_df, test=False)
+
+    user_X_df = preprocessed_X_df[:1]
+    write_user_data(user_X_df)
+
+    prediction, prediction_probas = load_model_and_predict(user_X_df)
+    write_prediction(prediction, prediction_probas)
+
+
+tab1, tab2, tab3 = st.tabs([
+    "Исследовать", "Предсказать", "Оценить"
+])
+
+
 if __name__ == "__main__":
-    process_main_page()
+    with tab2:
+        row1, row2 = st.columns([1, 3])
+        with row1:
+            compute_tab1()
+            user_input_df = input_features()
+        with row2:
+            process_side_bar_inputs()
+
+    with tab1:
+        df = open_data()
+        plot_age(df)
+        plot_postal_adress(df)
+        plot_education(df)
